4f9132bfc1e1759c92ca0c4deb35f5456ad01a10
gary1anthony <143620103+gary1anthony@users.noreply.github.com>
2023-10-09 04:18:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_ete0djm", "template_j4tg79b", formData)
+    emailjs.send("service_v8hvmeh", "template_vp2wsdr", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
